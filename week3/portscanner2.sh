31ee4f9111605104200a5276b20223768f3c353b
natekreit <nathan.kreit@mymail.champlain.edu>
2025-09-15 20:39:49 -0400

    update

---
+++ week3/portscanner2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ports=$2
+echo "ip,     port"
+for i in {1..254}; do
+	network=$1.$i
+	for ip in $network ; do
+		for port in $ports; do
+			timeout .1 bash -c "echo >/dev/tcp/$ip/$port" 2>/dev/null &&
+				echo "$ip,     $port"
+		done
+	done
+done
