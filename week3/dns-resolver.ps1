18fa2fe469545475bc6fb370d4c32aef7ce8b05d
natekreit <nathan.kreit@mymail.champlain.edu>
2025-09-15 23:17:30 -0400

    update

---
+++ week3/dns-resolver.ps1
@@ -0,0 +1,10 @@
+﻿param($network, $server)
+
+for ($i = 1 ; $i -lt 255 ; $i++){
+    $ipadd = ($network + "." + $i)
+    $hname = Resolve-DNSName -DnsOnly $ipadd -server $Server -ErrorAction Ignore
+    if ($null -ne $hname) {
+        Write-Host $ipadd $hname.NameHost
+    }
+
+}
